ce22bdd220463539b774df6226ea68634f4dc10b
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-11 15:01:16 +0600

    adding time in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
 const Header = () => {
+  const today = new Date();
+  const formatted = today.toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "long",
+    year: "numeric",
+    day: "numeric",
+  });
+  const day = formatted.split(",")[0];
+  const others = formatted.split(",").slice(1, 3);
   return (
-    <div className="text-center">
+    <div className="text-center lg:mt-[30px] mt-[20px]">
       <img
         className="lg:h-[60px] mx-auto"
         src="https://i.ibb.co.com/HvhWTfN/The-Dragon-News.png"
         alt=""
       />
-      <p>Journalism Without Fear or Favour</p>
-      <p>Sunday, November 27, 2022</p>
+      <p className="text-[#706f6f] text-[18px] mt-5 mb-2">
+        Journalism Without Fear or Favour
+      </p>
+      <p className="text-[18px]  font-[500]">
+        {day} <span className="text-[#706f6f]">{others}</span>
+      </p>
     </div>
   );
 };
