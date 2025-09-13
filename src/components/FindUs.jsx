c00a9876372b674c988388062238e1434b472afa
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-13 12:33:33 +0600

    updating findus

--- src/components/FindUs.jsx
+++ src/components/FindUs.jsx
@@ -5,7 +5,7 @@ const FindUs = () => {
     <div>
       <h3 className="text-xl font-semibold mt-[30px] mb-[22px]">Find Us On</h3>
       <div className="text-[#706f6f] font-[500] *:w-full text-[17px]">
-        <button className="flex items-center gap-3 border-1 border-[#e7e7e7]  p-4 rounded-t-[6px] cursor-pointer">
+        <button className="flex items-center gap-3 border-1 border-[#e7e7e7]  p-4 rounded-t-[4px] cursor-pointer">
           <div className="bg-[#f3f3f3] p-2 rounded-full">
             <FaFacebookF className="text-[#3b599c] rounded-full" />
           </div>{" "}
@@ -17,7 +17,7 @@ const FindUs = () => {
           </div>{" "}
           Twitter
         </button>
-        <button className="flex items-center gap-3 border-1 border-[#e7e7e7]  p-4 border-t-0 cursor-pointer">
+        <button className="flex items-center gap-3 border-1 border-[#e7e7e7]  p-4 border-t-0 cursor-pointer rounded-b-[4px]">
           <div className="bg-[#f3f3f3] p-2 rounded-full rounded-b-[6xl]">
             <FaInstagram className="rounded-full text-[linear-gradient(180deg,rgba(216,45,126,1)_6%,rgba(251,167,86,1)_100%)] bg-clip-text" />
           </div>{" "}
