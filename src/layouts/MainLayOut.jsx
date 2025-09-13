3f5d08840028dc2748dba62d9fd106bc591780f3
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-13 12:45:50 +0600

    adding footer

--- src/layouts/MainLayOut.jsx
+++ src/layouts/MainLayOut.jsx
@@ -1,4 +1,5 @@
 import { Outlet, useLocation } from "react-router-dom";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
 import Navbar from "../components/Navbar";
@@ -13,6 +14,7 @@ const MainLayOut = () => {
       <div>
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 };
