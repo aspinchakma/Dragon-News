306229f8454b70c980ce651189bfd1c6296d1bea
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-13 12:11:10 +0600

    dynamically adding the star in news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,9 +1,10 @@
-import { FaEye, FaRegBookmark } from "react-icons/fa";
+import { FaEye, FaRegBookmark, FaStar } from "react-icons/fa";
 import { IoMdShare } from "react-icons/io";
 import { Link } from "react-router-dom";
 
 const News = ({ singleNews }) => {
   const { image_url, title, author, details, total_view, _id } = singleNews;
+  const ratingNumber = Math.round(singleNews.rating.number);
   return (
     <div className="border-2 border-[#e7e7e7] rounded-lg single_news_container">
       <div className="flex justify-between items-center  bg-[#f3f3f3] p-4 rounded-t-lg ">
@@ -32,9 +33,19 @@ const News = ({ singleNews }) => {
         </Link>
         <div className="divider"></div>
         <div className=" flex items-center justify-between">
-          <div>
-            <h2>Okay</h2>
-          </div>
+          <ul className="flex items-center gap-1">
+            {[...new Array(5)].map((_, index) => {
+              return (
+                <li key={index}>
+                  <FaStar
+                    className={`${
+                      index < ratingNumber ? "text-[#ff8c47]" : "text-[#706f6f]"
+                    } text-[18px]`}
+                  />
+                </li>
+              );
+            })}
+          </ul>
           <div className="flex gap-2 items-center text-[#706f6f]">
             <FaEye className="text-[24px]"></FaEye>
             <h3 className="font-[500]">{total_view}</h3>
